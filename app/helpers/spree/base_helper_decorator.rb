fcfef8231a1d04d8ee362093ee4b0bcd122a78fc
dima <pronix.service@gmail.com>
2011-04-21 20:23:35 +0400

    fix

--- app/helpers/spree/base_helper_decorator.rb
+++ app/helpers/spree/base_helper_decorator.rb
@@ -1,11 +1,9 @@
 module Spree::BaseHelper
   def order_price(order, options={})
     options.assert_valid_keys(:format_as_currency, :show_vat_text, :show_price_inc_vat)
-    options.reverse_merge! :format_as_currency => true, :show_vat_text => true
-    options[:show_vat_text] = Spree::Config[:show_price_inc_vat]
-    amount = Currency.convert(order.item_total, order.currency, Currency.current.char_code)
-    Order.find(order.id).update_attribute("currency", Currency.current.char_code)
-    Order.find(order.id).update_attribute("item_total", amount)
+    options.reverse_merge! :format_as_currency => true, :show_vat_text => true, :show_vat_text => Spree::Config[:show_price_inc_vat]
+    ammount = Currency.convert(order.item_total, Currency.basic, Currency.current.char_code)
+    ammount += Calculator::Vat.calculate_tax(order) if Spree::Config[:show_price_inc_vat]
     options.delete(:format_as_currency) ? number_to_currency(amount) : amount
   end
 end
